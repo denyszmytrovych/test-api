62571b4bf81b191c87a14bdc3df4112c75b81647
Denys Zmytrovych <denys.zmytrovych@codeit.pro>
2021-05-06 14:20:39 +0300

    removed unused code

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/conversions', [ConversionController::class, 'index']);
 Route::post('/conversions/convert', [ConversionController::class, 'convert']);
 Route::get('/conversions/top', [ConversionController::class, 'top']);
-
-//Route::post('/conversions/convert', function () {
-//    return 545454;
-//});
